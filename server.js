34af055d847a5a95f42993a976b84929016511a9
Janos Csikos <csikosjanos@Janoss-Mac-mini.local>
2013-01-22 12:16:39 +0000

    step5: answer for static content from /public_html folder
    Restart express in the running terminal:
    ^C
    $ node ./server.js
    Listening on port 3000
    
    locate your browser to: http://localhost:3000/

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ var express = require('express');
 var app = express();
 
 
-app.get('/hello.txt', function(req, res){
-  res.send('Hello World');
-});
+app.use(express.static(__dirname + '/public_html'));
 
 app.listen(3000);
 console.log('Listening on port 3000');
